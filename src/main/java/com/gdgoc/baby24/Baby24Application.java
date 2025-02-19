38ae3c3ef34d0ac7c35dda7d354ee70334ee13a9
siyeonSon <siyeon0209@naver.com>
2025-02-19 20:08:06 +0900

    Initial commit

---
+++ src/main/java/com/gdgoc/baby24/Baby24Application.java
@@ -0,0 +1,13 @@
+package com.gdgoc.baby24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Baby24Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Baby24Application.class, args);
+    }
+
+}
