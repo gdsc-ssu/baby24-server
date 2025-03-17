879ada8fd35011c61c037a6f1e10e0d15b976d7e
sangeun <olsang1203@gmail.com>
2025-03-17 17:06:11 +0900

    feat: 알림용 기기 설정시 Light만 가능하도록 에러메시지 추가

--- src/main/java/com/gdgoc/baby24/common/exception/ErrorCode.java
+++ src/main/java/com/gdgoc/baby24/common/exception/ErrorCode.java
@@ -9,7 +9,10 @@ public enum ErrorCode {
     METHOD_NOT_ALLOWED(405, "C_002", "잘못된 HTTP Request Method 입니다."),
     INVALID_INPUT_VALUE(400, "C_003", "적절하지 않은 요청 값입니다."),
     INVALID_TYPE_VALUE(400, "C_004", "요청 값의 타입이 잘못되었습니다."),
-    ENTITY_NOT_FOUND(400, "C_005", "지정한 엔티티를 찾을 수 없습니다.")
+    ENTITY_NOT_FOUND(400, "C_005", "지정한 엔티티를 찾을 수 없습니다."),
+
+    //device
+    DEVICE_NOT_ALLOWED(400,"D_001","Light만 알림용으로 설정할 수 있습니다."),
     ;
 
     private final int status;
