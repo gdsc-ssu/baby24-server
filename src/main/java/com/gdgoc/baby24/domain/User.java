8fdec214268c2c2ddf1f2182f46bfed733eed37f
sangeun <olsang1203@gmail.com>
2025-03-17 23:43:33 +0900

    refactor: 접근 제어자 추가

--- src/main/java/com/gdgoc/baby24/domain/User.java
+++ src/main/java/com/gdgoc/baby24/domain/User.java
@@ -15,9 +15,9 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    String username;
-    String email;
-    String PAT;
+    private String username;
+    private String email;
+    private String personalAccessToken;
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private List<Device> reviews = new ArrayList<>();
 }
