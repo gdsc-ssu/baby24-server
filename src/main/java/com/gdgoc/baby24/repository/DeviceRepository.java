2c241e4d5606eda073ba0a0cfca978c4b62644aa
sangeun <olsang1203@gmail.com>
2025-03-17 15:43:54 +0900

    feat: entity별 repository 생성

---
+++ src/main/java/com/gdgoc/baby24/repository/DeviceRepository.java
@@ -0,0 +1,7 @@
+package com.gdgoc.baby24.repository;
+
+import com.gdgoc.baby24.domain.Device;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DeviceRepository extends JpaRepository<Device, Long> {
+}
