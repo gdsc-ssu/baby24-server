65134ec932db69a76ae016a1c67375e189928ef8
siyeonSon <siyeon0209@naver.com>
2025-03-20 23:12:40 +0900

    fix: spring security 진입 시 로그인 모달 제거

--- src/main/java/com/gdgoc/baby24/config/SecurityConfig.java
+++ src/main/java/com/gdgoc/baby24/config/SecurityConfig.java
@@ -2,8 +2,8 @@ package com.gdgoc.baby24.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -21,7 +21,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
-                .httpBasic(Customizer.withDefaults());
+                .httpBasic(AbstractHttpConfigurer::disable);
         return http.build();
     }
 
